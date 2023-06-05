e8b04f50a55900dbf842e4d54d890626228a1a63
owenppdokc <126684947+owenppdokc@users.noreply.github.com>
2023-06-04 20:31:25 -0600

    Se agregó la estructura de carro

--- carF.cpp
+++ carF.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <string.h>
+#define MAX 100
+
+using namespace std;
+
+typedef struct
+{
+    int id;
+    char kind[10];
+    char brand[10];
+    char model[15];
+    char color[10];
+    char transmission[5];
+    char year[4];
+    float price;
+} carData;
 
