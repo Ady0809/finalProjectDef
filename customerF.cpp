815d1dbd02e031e1c5e05597cce81085fa9f3c10
Ady0809 <126684945+Ady0809@users.noreply.github.com>
2023-06-04 20:09:33 -0600

    Se agrego showCustomer

--- customerF.cpp
+++ customerF.cpp
@@ -38,3 +38,19 @@ void addCustomer(customerData customer)
     customers[lastRecord] = customer;
     lastRecord++;
 }
+
+void showCustomer(int pos)
+{
+    cout << "_______________________________________________________________________________________________________________" << endl;
+    cout << "\t\t\t\tCUSTOMER INFO" << endl;
+    cout << "_______________________________________________________________________________________________________________" << endl;
+    cout << "|       ID       |      Name      |      Last name    |              Address          |           Email          |      Cellphone #      |" << endl;
+    cout << "_________________________________________________________________________________________________________________" << endl;
+    cout << "|        " << customers[pos].id << "       |      " << customers[pos].name << "   |        " << customers[pos].lastName << "|" << customers[pos].address << "|" << customers[pos].email << "|" << customers[pos].cellPhoneNumber << "|" << endl;
+    cout << "_______________________________________________________________________________________________________________" << endl;
+    cout << "\t\t\t\tCAR INFO" << endl;
+    cout << "_______________________________________________________________________________________________________________" << endl;
+    cout << "|       ID       |      Kind      |      Brand    |           Model          |           Color          |      Transmission      |      Year      |       Price     |" << endl;
+    cout << "|" << cars[pos].id << "|" << cars[pos].kind << "|" << cars[pos].brand << "|" << cars[pos].model << "|" << cars[pos].color << "|" << cars[pos].transmission << "|" << cars[pos].year << "|" << cars[pos].price << "|\n\n\n";
+    cout << "_______________________________________________________________________________________________________________" << endl;
+}
