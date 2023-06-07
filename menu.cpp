094cfb6b28d9d7a2c1544f32c7faa1c259bbc8d9
dichuu08 <126684926+dichuu08@users.noreply.github.com>
2023-06-06 23:14:02 -0600

    Se agrego menuCar completo

--- menu.cpp
+++ menu.cpp
@@ -219,3 +219,194 @@ void initialize()
     } while (choice != 8);
     saveCustomers();
 }
+
+void menuCar()
+{
+    int choice;
+    readCars();
+    do
+    {
+        system("cls");
+        cout << "*****CAR INVENTORY*****\n";
+        cout << "1. Add car to inventory" << endl;
+        cout << "2. Search car" << endl;
+        cout << "3. Show car inventory" << endl;
+        cout << "4. Edit car" << endl;
+        cout << "5. Delete car" << endl;
+        cout << "6. Exit\n";
+
+        cout << "\nEnter your choice: ";
+        cin >> choice;
+        cin.ignore();
+
+        system("cls");
+
+        switch (choice)
+        {
+        case 1:
+
+            car.id = lastCar+1;
+
+            cout << "ID: " << car.id << endl;
+
+            cout << "Kind: ";
+            scanf(" %[^\n]", car.kind);
+
+            cout << "Brand: ";
+            scanf(" %[^\n]", car.brand);
+
+            cout << "Model: ";
+            scanf(" %[^\n]", car.model);
+
+            cout << "Color: ";
+            scanf(" %[^\n]", car.color);
+
+            cout << "Transmission: ";
+            scanf(" %[^\n]", car.transmission);
+
+            cout << "Year: ";
+            scanf(" %[^\n]", car.year);
+
+            cout << "Price: ";
+            cin >> car.price;
+
+            addCar(car);
+
+            system("cls");
+            cout << "Car added successfully!\n\n";
+            system("pause");
+            break;
+
+        case 2:
+            cout << "Enter ID to search: ";
+            cin >> id;
+            cin.ignore();
+
+            pos = carExists(id);
+
+            if (pos == -1)
+            {
+                cout << "Car not found. Please try again" << endl;
+                system("pause");
+                break;
+            }
+            else
+            {
+                showCar(pos);
+            }
+
+            system("pause");
+            break;
+
+        case 3:
+            showCars();
+            system("pause");
+            break;
+
+        case 4:
+            cout << "Enter ID to edit: ";
+            cin >> id;
+            cin.ignore();
+
+            pos = carExists(id);
+
+            if (pos == -1)
+            {
+                cout << "Car not found. Please try again" << endl;
+                system("pause");
+                break;
+            }
+            else
+            {
+                showCar(pos);
+
+                cout << "\n\n\n_________________________________" << endl;
+                cout << "\tNew values" << endl;
+                cout << "_________________________________\n"
+                     << endl;
+                
+                car.id = pos+1;
+
+                cout<<"ID: "<< car.id<<endl;
+
+                cout << "Kind: ";   
+                scanf(" %[^\n]", car.kind);
+
+                cout << "Brand: ";
+                scanf(" %[^\n]", car.brand);
+
+                cout << "Model: ";
+                scanf(" %[^\n]", car.model);
+
+                cout << "Color: ";
+                scanf(" %[^\n]", car.color);
+
+                cout << "Transmission: ";
+                scanf(" %[^\n]", car.transmission);
+
+                cout << "Year: ";
+                scanf(" %[^\n]", car.year);
+
+                cout << "Price: ";
+                cin >> car.price;
+
+                editCar(car, pos);
+
+                system("pause");
+                break;
+            }
+
+        case 5:
+            if (lastCar == 0)
+            {
+                cout << "\nNo customers to delete\n" << endl;
+                system("pause");
+                break;
+            }
+
+            cout << "Enter ID to delete: ";
+            cin >> id;
+
+            pos = carExists(id);
+            
+            if (pos == -1)
+            {
+                cout << "Car not found. Please try again" << endl;
+            }
+            else
+            {
+                cout << "\n\n";
+
+                cout << "Are you sure you want to delete car '" << cars[pos].brand<< " " << cars[pos].model << "'? (y/n): ";
+                cin >> ans;
+                cin.ignore();
+
+                if (ans == "y" || ans == "Y")
+                {
+                    deleteCar(pos);
+                    cout << "Car deleted successfully!\n" << endl;
+                }
+                else if (ans == "n" || ans == "N")
+                {
+                    cout << "Action canceled successfully!\n";
+                }
+                else
+                {
+                    cout << "Unavailable choice. Please try again\n";
+                }
+
+                system("pause");
+                break;
+            }
+
+        case 6:
+            break;
+
+        default:
+            cout << "Unavailable choice" << endl;
+            system("pause");
+            break;
+        }
+    } while (choice != 6);
+    saveCars();
+}
