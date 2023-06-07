aa6d8dc4c4607ae7ec96611a2a499c263cd2f49f
dichuu08 <126684926+dichuu08@users.noreply.github.com>
2023-06-06 23:06:54 -0600

    Se agrego initialize completo

--- menu.cpp
+++ menu.cpp
@@ -102,3 +102,161 @@ void initialize()
         case 1:
             menuCar();
             break;
+
+        case 2:
+            system("cls");
+            
+            customer.id = lastRecord+1;
+            cout << "ID: " << customer.id << endl;
+            cout << "Name: ";
+            scanf(" %[^\n]", customer.name);
+            cout << "Last name: ";
+            scanf(" %[^\n]", customer.lastName);
+            cout << "Address: ";
+            scanf(" %[^\n]", customer.address);
+            cout << "Email: ";
+            scanf(" %[^\n]", customer.email);
+            cout << "Cellphone number: ";
+            scanf(" %[^\n]", customer.cellPhoneNumber);
+
+            system("cls");
+            cout << "Customer added successfully\n\n\n\n";
+
+            showCars();
+
+            cout << "Enter ID of the car bought: ";
+            cin >> id;
+
+            pos = carExists(id);
+            reqCar= getCar(pos);
+            
+            if(pos != -1)
+            {
+                customer.car = reqCar;
+            }
+            else
+            {
+                // Car doesn´t exists
+                cout << "Invalid car ID. Car not found." << endl;
+            }       
+
+            addCustomer(customer);
+
+            system("pause");
+            break;
+
+        case 3:
+            system("cls");
+            cout << "Enter ID to search: ";
+            cin >> id;
+            system("cls");
+            pos = customerExists(id);
+            if (pos == -1)
+            {
+                cout << "Customer not found" << endl;
+                system("pause");
+                break;
+            }
+            else
+            {
+                showCustomer(pos);
+            }
+            system("pause");
+            break;
+
+        case 4:
+        if(lastRecord == 0){
+            system("cls");
+            cout << "No customers registered" << endl;
+            system("pause");
+            break;
+        }
+            showCustomers();
+            system("pause");
+            break;
+
+        case 5:
+        system("cls");
+            cout << "Enter ID to edit: ";
+            cin.ignore();
+            cin >> id;
+
+            pos = customerExists(id);
+            showCustomer(pos);
+
+            cout << "\n\n\n_________________________________" << endl;
+            cout << "\tNew values" << endl;
+            cout << "_________________________________\n"
+                    << endl;
+            
+            customer.id = pos+1;
+
+            cout<<"ID: "<< customer.id <<endl;
+
+            cout << "Name: ";   
+            scanf(" %[^\n]", customer.name);
+
+            cout << "Last name: ";
+            scanf(" %[^\n]", customer.lastName);
+
+            cout << "Address: ";
+            scanf(" %[^\n]", customer.address);
+
+            cout << "Email: ";
+            scanf(" %[^\n]", customer.email);
+
+            cout << "Cellphone #: ";
+            scanf(" %[^\n]", customer.cellPhoneNumber);
+
+            editCustomer(customer, pos);
+
+            cout << "Customer edited succesfully!\n"
+                 << endl;
+            break;
+
+        case 6:
+            system("cls");
+            if (lastRecord == 0)
+            {
+                cout << "No customers to delete\n"
+                     << endl;
+            }
+
+            cout << "Enter ID to delete: ";
+            cin >> id;
+
+            pos = customerExists(id);
+
+            deleteCustomer(pos);
+            break;
+
+        case 7:
+            system("cls");
+            cout<<"Enter customer ID to generate bill: ";
+            cin>>id;
+
+            pos = customerExists(id);
+            reqCustomer = getCustomer(pos);
+
+            if (pos != -1)
+            {
+                customer = reqCustomer;
+                printBill(pos);
+                
+            } else {
+                cout<<"Customer not found"<<endl;
+            }
+            
+            system("pause");
+            break;
+
+        case 8:
+            break;
+
+        default:
+            cout << "Unavailable choice, please try again\n";
+            system("pause");
+            break;
+        }
+
+    } while (choice != 8);
