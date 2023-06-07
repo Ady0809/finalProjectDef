d161f3319fa456a6b85f48df28469d9a94c56eeb
Cristellaguna <126684295+Cristellaguna@users.noreply.github.com>
2023-06-06 21:57:01 -0600

    Se agregaron prototipos
    Se agregaron prototipos mainMenu, initialize, menuCar

---
+++ menu.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "customerF.cpp"
+#include"billF.cpp"
+
+using namespace std;
+
+void mainMenu();
+void initialize();
+void menuCar();
