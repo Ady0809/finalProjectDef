3b852381e41ba9c375d588b83334304a3084a282
Cristellaguna <126684295+Cristellaguna@users.noreply.github.com>
2023-06-06 21:59:51 -0600

    Se declaro el mainMenu

--- menu.cpp
+++ menu.cpp
@@ -19,3 +19,24 @@ carData car;
 carData reqCar;
 carData reqCar0;
 customerData reqCustomer;
+
+void mainMenu()
+{
+    int choice;
+
+    cout << "___________________________________________________" << endl;
+    cout << "\tCUSTOMER REGISTER SYSTEM" << endl;
+    cout << "\t   Current records: " << lastRecord << endl;
+    cout << "___________________________________________________\n"
+         << endl;
+
+    cout << "1. Car manage" << endl;
+    cout << "2. Add customer" << endl;
+    cout << "3. Search customer" << endl;
+    cout << "4. Show customers" << endl;
+    cout << "5. Edit customer" << endl;
+    cout << "6. Delete customer" << endl;
+    cout << "7. Generate bill" << endl;
+    cout << "8. Exit\n\n";
+    cout << "Enter your choice: ";
+}
